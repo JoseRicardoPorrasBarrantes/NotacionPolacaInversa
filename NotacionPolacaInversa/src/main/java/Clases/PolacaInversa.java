b95a0891f738ac598865013bd964738af21bf4c6
Vulkmir <jose.porras@ucrso.info>
2020-05-31 17:07:18 -0600

    revision cierre

--- NotacionPolacaInversa/src/main/java/Clases/PolacaInversa.java
+++ NotacionPolacaInversa/src/main/java/Clases/PolacaInversa.java
@@ -10,56 +10,63 @@ package Clases;
  * @author USER
  */
 public class PolacaInversa<T> {
-    
+
     Pila pila = new Pila(0);
     Cola cola = new Cola();
     Cola colaPostFija = new Cola();
-    
-    public void agregarNotacion(String ecuacion) throws Exception{
-    
-        for(int i = 0 ; i < ecuacion.length() ; i++){
+
+    public void agregarNotacion(String ecuacion) throws Exception {
+
+        for (int i = 0; i < ecuacion.length(); i++) {
             cola.encolar(ecuacion.charAt(i));
-     
+
         }
 
     }
-    
-   
-   public void dirigirElemento() throws VacioException, Exception{
-   
-       
-   while(cola.isEmpty() == false){
-   
-       char elemento = (char) cola.desencolar();
-       
-   if(Character.isDigit( elemento) == false && Character.isAlphabetic(elemento) == false){
-   
-       if(elemento == ')' || elemento == ']' || elemento == '}'){
-       
-       
-       }
-       
-       pila.push(elemento);
-    
-       
-//  System.out.println("Agregando a la pila " + elemento);
-   }else{
-   
-       colaPostFija.encolar(elemento);
-     //  System.out.println("Agregando a la cola : " + elemento);
-   }
 
-   }    
-       
-       
-   
-   } 
-    
-  public void revisionCierre(char t){}  
-    
-   
-   
-   
-    
-    
+    public void dirigirElemento() throws VacioException, Exception {
+
+        while (cola.isEmpty() == false) {
+
+            char elemento = (char) cola.desencolar();
+
+            if (Character.isDigit(elemento) == false && Character.isAlphabetic(elemento) == false) {
+
+                if (elemento == ')' || elemento == ']' || elemento == '}') {
+
+                }
+
+                pila.push(elemento);
+
+//  System.out.println("Agregando a la pila " + elemento);
+            } else {
+
+                colaPostFija.encolar(elemento);
+                //  System.out.println("Agregando a la cola : " + elemento);
+            }
+
+        }
+
+    }
+
+    public void revisionCierre(char t) throws Exception {
+
+        if (t == ')') {
+            while (pila.top() != '(') {
+                colaPostFija.encolar(pila.pop());
+            }
+
+        } else if (t == '}') {
+            while (pila.top() != '{') {
+                colaPostFija.encolar(pila.pop());
+            }
+
+        } else if (t == ']') {
+            while (pila.top() != '[') {
+                colaPostFija.encolar(pila.pop());
+            }
+        }
+
+    }
+
 }
