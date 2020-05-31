ced7909fde52abc15aefca9147595541b1634186
Roberto-sc <luis.solano@ucrso.info>
2020-05-31 15:22:45 -0600

    Agrega la excepcion

---
+++ NotacionPolacaInversa/src/main/java/Clases/MisExcepxiones.java
@@ -0,0 +1,21 @@
+package Clases;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class MisExcepxiones extends Exception{
+
+    public MisExcepxiones(String message) {
+        super(message);
+    }
+    
+    
+    
+}
